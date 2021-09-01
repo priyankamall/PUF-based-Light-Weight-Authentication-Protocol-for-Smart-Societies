2c73aa4aa4bb8a2a484404d62811e282239b14ab
Priyanka Mall <87891538+PMall09@users.noreply.github.com>
2021-09-01 14:20:30 +0530

    Update ScytherCodeB.py

--- ScytherCodeB.py
+++ ScytherCodeB.py
@@ -28,7 +28,7 @@ macro Rbc'= XOR(Obc,H(BIDr,BSr',Tbc));
 macro Nbc'=H(BIDr,BSr',Rbc');
 match(Nbc',Nbc);
 fresh Rcb;
-macro SKb-c = H(IDc,BIDr,Rbc,Rcb);
+macro SKb-c = H(IDc,BIDr,Rbc,Rcb);#Base station and cloud server session key
 macro Rc-b=XOR(Rcb,Rbc);
 macro Pcb=H(IDc,BSr,SKb-c,Tcb);
 send_!11(Cloud,BaseStation,Tbc,Rc-b,Pcb,IDc);
