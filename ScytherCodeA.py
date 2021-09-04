2dbe4a618110bc6880f9cc5fbcc14baa940aaa17
Priyanka Mall <87891538+PMall09@users.noreply.github.com>
2021-09-04 15:37:40 +0530

    Update ScytherCodeA.py

--- ScytherCodeA.py
+++ ScytherCodeA.py
@@ -37,7 +37,7 @@ const IDc;fresh Rc;
 macro BSr=H(BIDr,Sc);
 send_!2(Cloud,BaseStation,BSr);
 recv_!4(User,Cloud,IDu);#cloud receives the user identity 
-recv_!5(User,Cloud,H(IDu, BFIu));
+recv_!5(User,Cloud,H(IDu, BFIu));#cloud receives the user identity and biometric values
 macro Ac=H(H(IDu,BFIu),Sc);
 macro Bc=XOR(Ac, H(IDu,H(IDu,BFIu)));
 macro Dc=H(XOR(IDu,PWu),BFIu);
