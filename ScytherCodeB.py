f92c490182d399cbcf0a902d7f59569213b361df
Priyanka Mall <87891538+PMall09@users.noreply.github.com>
2021-09-04 15:43:21 +0530

    Update ScytherCodeB.py

--- ScytherCodeB.py
+++ ScytherCodeB.py
@@ -1,6 +1,7 @@
 protocol SmartSociety(User, Cloud,BaseStation,SensorNodeAndCamera){
 role User{
-const IDu;const PWu;const BFu;fresh Ras;# user identity
+const IDu;# user identity
+const PWu;const BFu;fresh Ras;
 macro CBc'=XOR(Ec,H(PWu',BFGu));
 macro Ku=H(IDu,CBc',RANu,T3);
 macro RANbs-u=XOR(RANu,H(CBc',T3));
