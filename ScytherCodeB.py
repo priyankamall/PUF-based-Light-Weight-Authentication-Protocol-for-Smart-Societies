62533067960192651e4c119fec9451f0f0fd9be0
Priyanka Mall <87891538+PMall09@users.noreply.github.com>
2021-09-15 17:08:52 +0530

    Update ScytherCodeB.py

--- ScytherCodeB.py
+++ ScytherCodeB.py
@@ -4,6 +4,7 @@ hashfunction H;
 const XOR:Function;
 const ADD:Function;     
 const MUL:Function;
+const GEN:Function;      
 protocol SmartSociety(User, Cloud,BaseStation,SensorNodeAndCamera){
 role User{
 const IDu;# user identity
