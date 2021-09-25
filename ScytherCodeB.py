023157d1dfa59ec389cada61cd35d75cc24337af
Priyanka Mall <87891538+PMall09@users.noreply.github.com>
2021-09-25 20:51:25 +0530

    Update ScytherCodeB.py

--- ScytherCodeB.py
+++ ScytherCodeB.py
@@ -87,7 +87,7 @@ match(XSs^,XSs); #verify by matching the constraints
 claim_BaseStation(BaseStation,Niagree); #non-injective agreement
 claim_BaseStation(BaseStation, Nisynch); #non-injective synchronization
 claim_BaseStation(BaseStation,Secret, RANu); #check the secrecy of the random number
-claim_BaseStation(BaseStation,Secret, SKbs-as);#session key secrecy verification
+claim_BaseStation(BaseStation,Secret, SKbs-as); #session key secrecy verification
 }
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%      
 role SensorNodeAndCamera{
