952ad6386807017395d499224e8ad6c21fd43fa4
Priyanka Mall <87891538+priyankamall@users.noreply.github.com>
2021-11-21 05:41:29 -0800

    Update ScytherCodeA.py

--- ScytherCodeA.py
+++ ScytherCodeA.py
@@ -4,7 +4,7 @@ hashfunction H;
 const XOR: Function;
 const ADD: Function;
 const MUL: Function;
-const GEN:Function; 
+const GEN: Function; 
 const BFIu,BFGu,Dc,Bc,T1,
 T2,IDc,Rc-as,Hc,Rc,T3,T4,
 PWu,PWu',
